5e1799589c1f21768096bba8ca6989024144bbde
seongsik082 <tjdtlr991210@gmail.com>
2024-07-01 22:44:43 +0900

    start

---
+++ board-admin/src/main/java/com/boardadmin/BoardAdminApplication.java
@@ -0,0 +1,13 @@
+package com.boardadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardAdminApplication.class, args);
+	}
+
+}
