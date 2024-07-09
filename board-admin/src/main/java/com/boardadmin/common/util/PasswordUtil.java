e50e0964fd19c03c1d9cda1dc01711a65e9666a0
msmj7 <msmj7@gmail.com>
2024-07-09 17:54:47 +0900

    사용자 삭제 수정

--- board-admin/src/main/java/com/boardadmin/common/util/PasswordUtil.java
+++ board-admin/src/main/java/com/boardadmin/common/util/PasswordUtil.java
@@ -9,9 +9,9 @@ public class PasswordUtil { // 중복인코딩방지 분리
     }
 
     public static String encodePassword(PasswordEncoder passwordEncoder, String rawPassword) {
-        if (rawPassword == null) {
-            throw new IllegalArgumentException("Password cannot be null");
-        }
+        //if (rawPassword == null) {
+        //    throw new IllegalArgumentException("Password cannot be null");
+       // }
         if (!rawPassword.startsWith("$2a$")) {
             return passwordEncoder.encode(rawPassword);
         }
