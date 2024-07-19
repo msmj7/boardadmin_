950ea7b900088431e80afadf685c39975f63a4c2
kss <tjdtlr991210@gmail.com>
2024-07-19 11:25:34 +0900

    main controller 추가/login controller '/'경로 삭제

---
+++ board-admin/src/main/java/com/boardadmin/board/controller/MainController.java
@@ -0,0 +1,24 @@
+package com.boardadmin.board.controller;
+
+import com.boardadmin.board.model.Board;
+import com.boardadmin.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class MainController {
+
+    @Autowired
+    private BoardService boardService;
+
+    @GetMapping("/")
+    public String mainPage(Model model) {
+        List<Board> boards = boardService.getAllBoards();
+        model.addAttribute("boards", boards);
+        return "main";
+    }
+}
