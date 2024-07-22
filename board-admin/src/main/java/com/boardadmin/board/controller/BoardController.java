8840f30b28c6a12eb362e79887258e649eac872b
kss <tjdtlr991210@gmail.com>
2024-07-22 14:40:28 +0900

    페이지네이션 수정

--- board-admin/src/main/java/com/boardadmin/board/controller/BoardController.java
+++ board-admin/src/main/java/com/boardadmin/board/controller/BoardController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Controller
-@RequestMapping("/boards")
+@RequestMapping("/admin/boards")
 public class BoardController {
 
     @Autowired
--- board-admin/src/main/java/com/boardadmin/board/controller/PostController.java
+++ board-admin/src/main/java/com/boardadmin/board/controller/PostController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/posts")
+@RequestMapping("/admin/posts")
 public class PostController {
 
     @Autowired
